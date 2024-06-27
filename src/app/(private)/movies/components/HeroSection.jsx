bdb502e51cc6d03a924782613e52a19a9388b9aa
merve <merveaydin86@hotmail.com>
2024-06-27 17:27:24 +0300

    VideoKey

--- src/app/(private)/movies/components/HeroSection.jsx
+++ src/app/(private)/movies/components/HeroSection.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import VideoSection from './VldeoSection';
+import React from "react";
+import VideoSection from "./VideoSection";
+import { getVideoKey } from "@/helpers/movieFunctions";
 
-const HeroSection = ({id}) => {
+const HeroSection = async ({ id }) => {
+  const videoKey = await getVideoKey(id);
+  console.log(videoKey);
   return (
     <div className="relative h-[50vw]">
-      <VideoSection />
-      
+      <VideoSection videoKey={videoKey} />
     </div>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
